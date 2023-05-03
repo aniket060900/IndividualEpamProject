5e088c0243f0e07562a7962050bc37ebdf56a544
Aniket <aniketkar96@gmail.com>
2023-05-03 23:45:13 +0530

    commited

---
+++ src/test/java/org/example/GoogleCloudTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+
+import com.Browser.FactoryBrowser;
+import org.openqa.selenium.*;
+import org.testng.annotations.Test;
+public class GoogleCloudTest {
+    @Test
+    public void GoogleCloud() throws InterruptedException {
+        WebDriver driver= FactoryBrowser.setupBrowser("chrome","https://cloud.google.com/");
+
+        GoogleCloudSecond calculatorPage=new GoogleCloudSecond(driver);
+        GoogleCloudFirst Cloud = new GoogleCloudFirst(driver);
+        Cloud.homePage();
+        Cloud.sendKeyInToNumberOfInstancesField("4");
+        Cloud.selectSeriesOfMachine();
+        Cloud.selectMachineType();
+        Cloud.clickAddGpusCheckBox();
+        Cloud.selectGpuType();
+        Cloud.selectNumberOfGpus();
+        Cloud.selectLocalSsd();
+        Cloud.selectDataCenterLocation();
+        Cloud.selectCommittedUsage();
+        calculatorPage.pushAddToEstimate();
+    }
+}
